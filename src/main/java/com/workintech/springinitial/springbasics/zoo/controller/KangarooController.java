5a5fdbb40452645893938d2868afd454df9f2a9e
Murat Simsek <muratsimseekk@gmail.com>
2024-02-09 01:41:22 +0300

    complete REST requests for Kangaroo

--- src/main/java/com/workintech/springinitial/springbasics/zoo/controller/KangarooController.java
+++ src/main/java/com/workintech/springinitial/springbasics/zoo/controller/KangarooController.java
@@ -2,10 +2,10 @@ package com.workintech.springinitial.springbasics.zoo.controller;
 
 import com.workintech.springinitial.springbasics.zoo.entity.Kangaroo;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,5 +16,31 @@ public class KangarooController {
     @PostConstruct
     public void init(){
         kangaroos=new HashMap<>();
+    }
+    @GetMapping("/kangaroos")
+    public List<Kangaroo> getKangaroos(){
+        return kangaroos.values().stream().toList();
+    }
+    @GetMapping("/kangaroos/{id}")
+    public Kangaroo getKangarooByID(@PathVariable Integer id){
+        return kangaroos.get(id);
+    }
+
+    @PostMapping("/kangaroos")
+    public Kangaroo addKangaroo(@RequestBody Kangaroo kangaroo){
+        kangaroos.put(kangaroo.getId(),kangaroo);
+        return kangaroo;
+    }
+    @PutMapping("/kangaroos/{id}")
+    public Kangaroo updateKangaroo(@RequestBody Kangaroo kangaroo,@PathVariable Integer id){
+        Kangaroo matchedKangaroo = kangaroos.get(id);
+         kangaroos.put(kangaroo.getId(),kangaroo);
+         return kangaroos.get(id);
+    }
+
+    @DeleteMapping("/kangaroos/{id}")
+    public void deleteKangaroo(@PathVariable Integer id){
+         kangaroos.remove(id);
+
     }
 }
